ad58fa6b3e6bda497d259ef8629f262d559e0ee3
Omar Bonilla <omar.bonilla@hotmail.com>
2017-08-01 18:48:54 -0500

    Incorporacion de librerias ckeditor e integracion para funcionalidad offline

--- application/views/cabecera.php
+++ application/views/cabecera.php
@@ -21,13 +21,13 @@ if(in_array($controlador, $this->session->userdata('permisos')) == FALSE){
     <head>
         <meta charset="utf-8">
         <title>Proyectando</title>
-        <script src="//cdn.ckeditor.com/4.6.2/basic/ckeditor.js"></script>
+        <script src="<?=base_url()?>application/libraries/ckeditor/ckeditor.js"></script>
         <!-- Latest compiled and minified CSS -->
         <link rel="stylesheet" href="<?=base_url()?>application/asset/bootstrap/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
         <link rel="stylesheet" href="<?=base_url()?>application/asset/css/estilos.css">
-        <script type='text/javascript' src="//ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
+        <script type='text/javascript' src="<?=base_url()?>application/asset/jquery/jquery-3.2.1.js"></script>
 
-       <script src="http://www.jqueryscript.net/demo/Export-Html-Table-To-Excel-Spreadsheet-using-jQuery-table2excel/src/jquery.table2excel.js"></script>
+      
     </head>
     <body>
         <div class="container-fluid">
