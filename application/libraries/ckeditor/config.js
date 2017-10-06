4b31ec1eb57127083b408fa0f6ba706068517940
Omar Maquina <johndoe@example.com>
2017-10-06 10:09:41 -0700

    Ajuste de editor de texto sin botones

--- application/libraries/ckeditor/config.js
+++ application/libraries/ckeditor/config.js
@@ -6,5 +6,17 @@
 CKEDITOR.editorConfig = function( config ) {
 	// Define changes to default configuration here. For example:
 	// config.language = 'fr';
-	// config.uiColor = '#AADC6E';
+   //  config.uiColor = '#AADC6E';
+	//config.removeButtons = 'Underline,Subscript,Superscript,Strike';
+	//config.removePlugins =  'elementspath,enterkey,entities,forms,pastefromword,htmldataprocessor,specialchar,horizontalrule,wsc' ;
+
+       //config.toolbar = 'Basic';
+       config.toolbar = [
+       ['Styles','Format','Font','FontSize'],
+       '/',
+       ['Bold','Italic','Underline','StrikeThrough','-','Undo','Redo','-','Cut','Copy','Paste','Find','Replace','-','Outdent','Indent','-','Print'],
+       '/',
+       ['NumberedList','BulletedList','-','JustifyLeft','JustifyCenter','JustifyRight','JustifyBlock'],
+       ['Image','Table','-','Link','TextColor','BGColor']
+    ] ;
 };
