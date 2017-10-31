fdf74583aaa63d1dea8885133aa20d7dce01aaa5
Omar Bonilla <omar.bonilla@hotmail.com>
2017-10-30 19:24:19 -0500

    CORREOS

--- application/controllers/Banco.php
+++ application/controllers/Banco.php
@@ -50,10 +50,39 @@ class Banco extends CI_Controller {
         $estado = $this->uri->segment(5, 0);
         $integrantes = $this->banco_model->mostrarIntegrantes($idea);
         $integran = array();
+        $correos = array();
         foreach ($integrantes->result() as $integrante) {
             $usuario = $integrante->id_usuario;
-            array_push($integran, $usuario);
+            $correo = $this->banco_model->mostrarCorreos($usuario);
+            if($correo!=null){
+           array_push($correos, $correo);
+            }
+           array_push($integran, $usuario);
         }
+        
+        $this->load->library('email');
+        $config['protocol'] = 'smtp';
+        $config["smtp_host"] = 'smtp.gmail.com';
+        $config["smtp_user"] = 'kptzmusic@gmail.com';
+        $config["smtp_pass"] = 'Admin2016';  
+        $config["smtp_port"] = '465';
+        $config['charset'] = 'utf-8';
+        $config['wordwrap'] = TRUE;
+        $config['validate'] = true;
+        $this->email->initialize($config);
+        $this->email->from('correo@gmail.com', 'Correos Proyectando');
+        $this->email->to('correo@gmail.com', 'Víctor Robles');
+        $this->email->subject("APROBACION");
+        $this->email->message(
+                "Email: adsadsadasdsadasdsad");
+        //Enviamos el email y si se produce bien o mal que avise con una flasdata
+        $mensajeenvio="";
+        if($this->email->send()){
+           $mensajeenvio = 'Se notifico a los integrantes';
+        }else{
+           $mensajeenvio = 'Error en el envio';
+        }
+        
         if ($estado == 3) {
             $cierreIdeas = $this->banco_model->mostrarIntegrantesConteo($idea, $integran);
             foreach ($cierreIdeas->result() as $retun) {
@@ -63,7 +92,7 @@ class Banco extends CI_Controller {
         }
         $this->banco_model->aprobarIdea($idea, $estado);
 
-        $this->session->set_flashdata('correcto', 'Idea Clasificada Correctamente!');
+        $this->session->set_flashdata('correcto', 'Idea Clasificada Correctamente '.$mensajeenvio.'!');
         redirect(base_url() . 'banco/bancoIdeas/' . $grupo, 'refresh');
     }
 
